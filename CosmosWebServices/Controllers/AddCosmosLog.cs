c6c1f498b034caa8f95b8baf241a494e366ca83c
Succi, Paolo <P205934@surrey.ca>
2023-03-16 14:23:23 -0700

    AddCosmosLog

--- CosmosWebServices/Controllers/AddCosmosLog.cs
+++ CosmosWebServices/Controllers/AddCosmosLog.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Principal;
 
 namespace CosmosWebServices.Controllers
 {
@@ -12,6 +13,10 @@ namespace CosmosWebServices.Controllers
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] CosmosLog cosmosLog, [FromServices] MyDbContext context)
         {
+            var sql = $@"INSERT INTO SPATIAL.COSMOS_LOG (ID, SOURCE, START_TIME, TOOL, USER_ID, TOOL_NAME)
+             VALUES (SEQ_COMSOS_LOG.NEXTVAL, '{cosmosLog.Source}', '{cosmosLog.StartTime}', '{cosmosLog.MapTheme}', '{cosmosLog.UserId}', '{cosmosLog.ToolName}')";
+
+            await context.Database.ExecuteSqlRawAsync(sql);
             return Ok();
         }
     }
--- CosmosWebServices/Models/CosmosLog.cs
+++ CosmosWebServices/Models/CosmosLog.cs
@@ -3,16 +3,21 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CosmosWebServices.Models
 {
-    [Table("COSMOS_LOG", Schema="SPATIAL")]
+    [Table("COSMOS_LOG", Schema = "SPATIAL")]
     public class CosmosLog
     {
         [Key]
-        [Column ("ID")]
+        [Column("ID")]
         public int LogId { get; set; }
         [Column("SOURCE")]
         public string? Source { get; set; }
+        [NotMapped]
+        private DateTime _startTime = DateTime.Now;
         [Column("START_TIME")]
-        public DateTime StartTime { get; set; } = DateTime.Now;
+        public string StartTime
+        {
+            get => _startTime.ToString("yyyy-MM-dd HH:mm:ss");
+        }
         [Column("USER_ID")]
         public string? UserId { get; set; }
         [Column("TOOL")]
